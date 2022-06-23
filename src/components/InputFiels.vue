82b64b13c2bdb00374c910a741e77c7fbe240483
Tushar-Mazhar <tushar.jatri@gmail.com>
2022-06-23 18:43:59 +0600

    add info and get info done

---
+++ src/components/InputFiels.vue
@@ -0,0 +1,120 @@
+<script setup>
+import {ref, onMounted} from 'vue'
+import {collection,onSnapshot,addDoc} from 'firebase/firestore'
+import {db} from '@/firebase'
+const getAllMasjidInformations = ref([])
+const information = ref({
+    Option:'',
+    Name:'',
+    Phone: '',
+    Address:'',
+    Product:'',
+    Amount:'',
+    Date: String(new Date())
+})
+
+    
+const informationStoreHandler = async ()=>{
+        await addDoc(collection(db,'totaldevelopmentamount'),{
+            Option: information.value.Option,
+            Name: information.value.Name,
+            Phone: information.value.Phone,
+            Address: information.value.Address,
+            Product: information.value.Product,
+            Amount: information.value.Amount,
+            Date: information.value.Date
+        })
+}
+
+const getAllMasjidInformation = ()=>{
+    onSnapshot(collection(db,'totaldevelopmentamount'),(querySnapShot)=>{
+    getAllMasjidInformations.value = []
+    querySnapShot.forEach(doc=>{
+        let data = {
+            id: doc.id,
+            Option: doc.data().Option,
+            Name: doc.data().Name,
+            Phone: doc.data().Phone,
+            Address: doc.data().Address,
+            Product: doc.data().Product,
+            Amount: doc.data().Amount,
+            Date: doc.data().Date,
+        }
+        getAllMasjidInformations.value.push(data)
+    })
+    })
+}
+
+onMounted(()=>{
+    getAllMasjidInformation()
+})
+
+</script>
+
+
+<template>
+    <div class="mainDiv">
+        <form class="form" @submit.prevent="informationStoreHandler">
+        <legend>মসজিদের সকল তথ্য জমা করুন</legend>
+        <div class="mb-3">
+            <label for="disabledSelect" class="form-label"></label>
+            <select default id="disabledSelect" class="form-select" v-model="information.Option">
+                <option  selected disabled value="">অপশন সিলেক্ট করুন</option>
+                <option value="জুম্মার দিনে দান বক্সে জমা">জুম্মার দিনে দান বক্সে জমা</option>
+                <option value="জুম্মার দিনে জিনিস-পত্র বিক্রয়">জুম্মার দিনে জিনিস-পত্র বিক্রয়</option>
+                <option value="মসজিদের উন্নয়ন/অনুষ্টানের জন্য জমা">মসজিদের উন্নয়ন/অনুষ্টানের জন্য জমা</option>
+            </select>
+        </div>
+        <div class="mb-3" v-if="information.Option!=''  && (information.Option==='জুম্মার দিনে জিনিস-পত্র বিক্রয়' || information.Option==='মসজিদের উন্নয়ন/অনুষ্টানের জন্য জমা')">
+            <label for="TextInput1" class="form-label">নাম লিখুন</label>
+            <input v-model="information.Name" required type="text" id="TextInput1" class="form-control" placeholder="নাম লিখুন">
+        </div>
+        <div class="mb-3" v-if="information.Option!='' && (information.Option==='জুম্মার দিনে জিনিস-পত্র বিক্রয়' || information.Option==='মসজিদের উন্নয়ন/অনুষ্টানের জন্য জমা')">
+            <label for="TextInput2" class="form-label">মোবাইল নাম্বার লিখুন</label>
+            <input v-model.number="information.Phone" required type="number" id="TextInput2" class="form-control" placeholder="মোবাইল নাম্বার লিখুন">
+        </div>
+        <div class="mb-3" v-if="information.Option!='' && (information.Option==='জুম্মার দিনে জিনিস-পত্র বিক্রয়' || information.Option==='মসজিদের উন্নয়ন/অনুষ্টানের জন্য জমা')">
+            <label for="TextInput3" class="form-label">ঠিকানা লিখুন</label>
+            <input v-model="information.Address" required type="text" id="TextInput3" class="form-control" placeholder="ঠিকানা লিখুন">
+        </div>
+        <div class="mb-3" v-if="information.Option!='' &&  information.Option==='জুম্মার দিনে জিনিস-পত্র বিক্রয়'">
+            <label for="TextInput4" class="form-label">জিনিসের বিবরণ লিখুন</label>
+            <textarea v-model="information.Product" required type="text" id="TextInput4" class="form-control" placeholder="জিনিসের বিবরণ লিখুন"></textarea>
+        </div>
+        <div class="mb-3" v-if="information.Option!='' && (information.Option==='জুম্মার দিনে দান বক্সে জমা' || information.Option==='জুম্মার দিনে জিনিস-পত্র বিক্রয়' || information.Option==='মসজিদের উন্নয়ন/অনুষ্টানের জন্য জমা')">
+            <label for="TextInput5" class="form-label">টাকার পরিমাণ লিখুন</label>
+            <input v-model.number="information.Amount" required type="number" id="TextInput5" class="form-control" placeholder="টাকার পরিমাণ লিখুন">
+        </div>
+        <div v-if="information.Option!=''">
+            <Datepicker v-model="information.Date" required></Datepicker>
+        </div>
+        <div v-if="information.Option!=''">
+            <button type="submit" class="btn btn-primary btnWidth mt-4">Submit</button>
+        </div>
+    </form>
+    </div>
+
+    <div>
+        <ul v-for="data in getAllMasjidInformations" :key="data.id">
+        <li>
+            {{data}}
+            </li>
+        </ul>
+    </div>
+
+</template>
+
+<style scoped>
+.form{
+    width:400px;
+    margin: 0 auto;
+}
+.mainDiv{
+   background-color: rgb(215, 215, 215);
+   padding: 40px;
+   box-sizing: border-box;
+}
+.btnWidth{
+    width:100%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('Datepicker', Datepicker);
 
 app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
 
